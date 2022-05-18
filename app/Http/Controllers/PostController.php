db18f505874f0015953d6d3cd65a0946bcf227bd
aipocha <aiko2525cute@icloud.com>
2022-05-18 19:57:01 +0900

    [add]ブログ投稿の編集

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -2,8 +2,10 @@
 
 namespace App\Http\Controllers;
 
+use App\Category;
 use App\Post;
 use App\Http\Requests\PostRequest;
+use Illuminate\Http\Request;
 
 class PostController extends Controller
 {
@@ -11,15 +13,30 @@ class PostController extends Controller
    {
     return view('posts/index')->with(['posts' => $post->getPaginateByLimit()]);
    }
+   
    public function show(Post $post)
    {
     return view('posts/show')->with(['post' => $post]);
    }
+   
    public function create()
    {
     return view('posts/create');
    }
-   public function store(Post $post, PostRequest $request)
+   
+   public function store(PostRequest $request, Post $post)
+   {
+    $input = $request['post'];
+    $post->fill($input)->save();
+    return redirect('/posts/' . $post->id);
+   }
+   
+   public function edit(Post $post)
+   {
+    return view('posts/edit')->with(['post' => $post]);
+   }
+   
+   public function update(Request $request, Post $post)
    {
     $input = $request['post'];
     $post->fill($input)->save();
