80aa457d285ef7c0c97f9e97f5429e588c6cb794
aipocha <aiko2525cute@icloud.com>
2022-05-19 19:23:21 +0900

    [add]ブログ投稿の削除

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -2,20 +2,22 @@
 <html lang="{{ str_replace("_", "-", app()->getLocale()) }}">
     <head>
         <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
         <title>Posts</title>
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
-        <link rel="stylesheet" href="/css/app.css">
+        <link rel="stylsheet" href="/css/app.css">
     </head>
     <body>
-        <h1 class="title">
-            {{ $post->title }}
-        </h1>
+        <h1 class="title">Blog</h1>
         <p class="edit">[<a href="/posts/{{ $post->id }}/edit">edit</a>]</p>
+        <form action="/posts/{{ $post->id }}" id="form_delete{{ $post->id }}" method="post" style="display:inline">
+          @csrf
+          @method('DELETE')
+          <button type="submit" onclick="return deletePost(this);">delete</button> 
+        </form>
         <div class="content">
             <div class="content__post">
-                <h3>本文</h3>
+                <h2>{{ $post->title }}</h2>
                 <p>{{ $post->body }}</p>    
                 <p>{{ $post->updated_at }}</p>    
             </div>
@@ -23,5 +25,13 @@
         <div class="footer">
             <a href="/">戻る</a>
         </div>
+        <script>
+        function deletePost(e) {
+            'use strict';
+            if (confirm('削除すると復元できません。\n本当に削除しますか？')){
+                document.getElementByID('form_delete').submit();
+            }
+        }
+        </script>
     </body>
 </html>
